f7f6606592128827543379c1f60df68034f3b858
SanExpett <alexkrasnoperov09@gmail.com>
2023-09-29 12:20:54 +0300

    добавил тестов

--- calculate/calculate_test.go
+++ calculate/calculate_test.go
@@ -33,4 +33,28 @@ func TestCalculate(t *testing.T) {
 	input = "-(-4*-10)"
 	actual, _ = calculate.Calculate(input)
 	assert.Equal(t, "-40", actual)
+
+	input = "7.5*-3.1"
+	actual, _ = calculate.Calculate(input)
+	assert.Equal(t, "-23.25", actual)
+
+	input = "4.5*-2"
+	actual, _ = calculate.Calculate(input)
+	assert.Equal(t, "-9", actual)
+}
+
+func TestCalulateErrors(t *testing.T){
+	t.Parallel()
+
+	input := "(1+2)-aaaaaaaaaaaaaaaaaaaaaaaaaa3"
+	_, err := calculate.Calculate(input)
+	assert.NotNil(t, err)
+
+	input = "(1+2)-3)"
+	_, err = calculate.Calculate(input)
+	assert.NotNil(t, err)
+
+	input = ""
+	_, err = calculate.Calculate(input)
+	assert.NotNil(t, err)
 }
